3b01c3deba3a11c393db8db94414d05dfd903346
Scott <scott@scottbenton.dev>
2024-08-10 20:15:31 -0400

    fix(runtime): Setting trustHost to true

--- src/auth.ts
+++ src/auth.ts
@@ -2,5 +2,6 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import google from '@auth/sveltekit/providers/google';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+	trustHost: true,
 	providers: [google]
 });
